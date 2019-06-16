fa9f90a8ca065ddcb5aa9638eddce9884180e9c8
Stefano Locati <stivlo@gmail.com>
2019-06-16 20:09:32 +0100

    Update a user and update a task endpoints

--- src/index.js
+++ src/index.js
@@ -43,6 +43,31 @@ app.get('/users/:id', async (req, res) => {
     }
 });
 
+// update a user
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+    if (!isValidOperation) {
+        return res.status(400).send({
+            error: 'Trying to update non-allowed field. Allowed fields: ' + allowedUpdates
+        });
+    }
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body,
+            {
+                new: true, // return the updated user
+                runValidators: true  // validate the user
+            });
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
 // create task
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
@@ -75,6 +100,30 @@ app.get('/tasks/:id', async (req, res) => {
         res.send(task);
     } catch (error) {
         res.status(500).send(error);
+    }
+});
+
+// update a task
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedFields = ['description', 'completed'];
+    const isValidOperation = updates.every(update => allowedFields.includes(update));
+    if (!isValidOperation) {
+        return res.status(400).send({
+            error: 'Non-allowed field. Allowed fields: ' + allowedFields
+        });
+    }
+    try {
+        const task = await Task.findByIdAndUpdate(
+            req.params.id, 
+            req.body,
+            { new: true, runValidators: true });
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch (error) {
+        res.status(400).send(error);
     }
 });
 
