f2b949c8514fb5be3fe211392b420b0739c3563c
Stefano Locati <stivlo@gmail.com>
2019-06-16 22:29:51 +0100

    refactored to have separate routers per resource (users and tasks)

---
+++ src/routers/task.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const router = new express.Router();
+const Task = require('../models/task');
+
+// create task
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body);
+    try {
+        await task.save();
+        res.status(201).send(task);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// find all tasks
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.send(tasks);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// find a task by id
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+    try {
+        const task = await Task.findById(_id);
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// update a task
+router.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedFields = ['description', 'completed'];
+    const isValidOperation = updates.every(update => allowedFields.includes(update));
+    if (!isValidOperation) {
+        return res.status(400).send({
+            error: 'Non-allowed field. Allowed fields: ' + allowedFields
+        });
+    }
+    try {
+        const task = await Task.findByIdAndUpdate(
+            req.params.id, 
+            req.body,
+            { new: true, runValidators: true });
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// delete a task
+router.delete("/tasks/:id", async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if (!task) {
+            res.status(404).send();
+        }
+        res.send(task);
+    } catch (error) {
+        res.status(500).send();
+    }
+});
+
+module.exports = router;
---
+++ src/routers/user.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const router = new express.Router();
+const User = require('../models/user');
+
+// create a user
+router.post('/users', async (req, res) => {
+    const user = new User(req.body);
+    try {
+        await user.save();
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// find all users
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// find a user by id
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id;
+    try {
+        const user = await User.findById(_id);
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// update a user
+router.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+    if (!isValidOperation) {
+        return res.status(400).send({
+            error: 'Trying to update non-allowed field. Allowed fields: ' + allowedUpdates
+        });
+    }
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body,
+            {
+                new: true, // return the updated user
+                runValidators: true  // validate the user
+            });
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// delete a user
+router.delete("/users/:id", async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            res.status(404).send();
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(500).send();
+    }
+});
+
+module.exports = router;
