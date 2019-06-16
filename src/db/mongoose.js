04222307854f8017c56bbfca2ea53ff29933cb8e
Stefano Locati <stivlo@gmail.com>
2019-06-16 16:12:06 +0100

    resolving deprecation warning

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,6 +4,7 @@ const dbConnectionUrl = 'mongodb://127.0.0.1:27017/task-manager-api';
 
 mongoose.connect(dbConnectionUrl, {
     useNewUrlParser: true,
+    useFindAndModify: false,
     useCreateIndex: true
 });
 
