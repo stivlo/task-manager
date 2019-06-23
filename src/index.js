411cd2d223e5a6c40c08548b301725b7ac66ed27
Stefano Locati <stivlo@gmail.com>
2019-06-23 13:25:36 +0100

    login user tests

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const app = require('./app');
 
 const port = process.env.port;
 app.listen(port, () => {
-    console.log('Server is up on port ' + port);
+    console.log('Task Manager App. Listening on port ' + port);
 });
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/models/user');
+
+const userOne = {
+    name: 'Mike',
+    email: 'mike@example.com',
+    password: '83248What?'
+};
+
+beforeEach(async () => {
+    await User.deleteMany();
+    await new User(userOne).save();
+});
 
 test('Should sign up a new user', async () => {
     await request(app).post('/users').send({
@@ -7,4 +19,18 @@ test('Should sign up a new user', async () => {
         email: 'andrew@example.com',
         password: 'Mypass7329!!'
     }).expect(201);
+});
+
+test('Should log in existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200);
+});
+
+test('Should not log in non existent user', async () => {
+    await request(app).post('/users/login').send({
+        email: 'unknown@example.com',
+        password: 'aPasswored!234'
+    }).expect(400);
 });
